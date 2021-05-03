4219c192621a24d8032bef4b5af158d6b820fcac
apexnemo <82320462+apexnemo@users.noreply.github.com>
2021-05-03 20:20:31 +0300

    Add files via upload

---
+++ gametest/sources/html/head.php
@@ -0,0 +1,64 @@
+<div><div class="noselect shadow_wrap hard" style="
+    width: 150px;
+    margin: 0 auto;
+    color: #fff;
+"><div class="relative" style="
+    background: #111;
+    height: 150px;pointer-events: none;
+    border-radius: 40px;
+    text-align: center;
+">
+<div class="th_out">
+<div class="th_in">
+<div style="
+    font-family: i;
+    color: #ffe7a4;
+    font-size: 60px;
+    line-height: 0.9;
+">ИГРО</div>
+<div style="
+    font-weight: 900;
+    letter-spacing: -1px;
+    font-size: 25.7px;
+    line-height: 0.8;
+    position: relative;
+    top: -4px;
+    color: #ffe7a4;
+">марафон</div><div style="
+    font-weight: 600;
+    position: absolute;
+    width: 100%;
+    font-size: 14px;
+    bottom: 15px;
+    opacity: 0.4;
+">v1.2</div>
+</div>
+</div>
+
+</div></div>
+
+<div class="resptxt" style="text-align: center; font-size: 15.1429px;">
+<div class="igrotitle" style="font-size: 5.5em;font-family: i;line-height: 1;padding: 20px 0;">ИГРОМАРАФОН</div>
+</div>
+<div style="
+    font-weight: 600;
+    text-align: center;
+    line-height: 1;
+    max-width: 360px;
+    margin: 0 auto;
+">Сценарий марафона c игрофикацией для онлайн-школ</div>
+<div style="
+    text-align: center;
+"><div style="
+    background: #111;
+    display: inline-block;
+    padding: 4px 10px;
+    margin-top: 6px;
+    border-radius: 5px;
+    line-height: 1;
+    color: #fff;
+    font-weight: 600;
+">v1.2</div></div>
+<style>
+.fr5574 {padding: 0 0 20px;border-bottom: 1px dashed rgba(0,0,0,0.2);}
+</style></div>
